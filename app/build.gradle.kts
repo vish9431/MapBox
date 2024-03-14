f0b55ebad6fb2a413c0e3780c0dd043827333dd2
vish9431 <vishnu21bcs19@iiitkottayam.ac.in>
2024-03-14 23:22:46 +0530

    Final Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -34,18 +34,39 @@ android {
         jvmTarget = "1.8"
     }
 }
+//
+//dependencies {
+//
+//    implementation("androidx.core:core-ktx:1.12.0")
+//    implementation("androidx.appcompat:appcompat:1.6.1")
+//    implementation("com.google.android.material:material:1.11.0")
+//    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+//    implementation("com.google.android.gms:play-services-maps:18.2.0")
+//    testImplementation("junit:junit:4.13.2")
+//    androidTestImplementation("androidx.test.ext:junit:1.1.5")
+//    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+//
+//    implementation ("com.google.android.material:material:1.11.0")
+//
+//
+//    implementation("com.mapbox.maps:android:11.2.0")
+//    implementation ("com.google.android.gms:play-services-location:21.2.0")
+//}
 
 dependencies {
-
+    // Core dependencies
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation("com.google.android.gms:play-services-maps:18.2.0")
+    implementation("com.google.android.gms:play-services-location:21.2.0")
+
+    // Testing dependencies
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
 
-    implementation ("com.google.android.material:material:1.3.0-alpha03")
-
+    // Mapbox SDK
     implementation("com.mapbox.maps:android:11.2.0")
 }
--- app/src/main/java/com/mapbox/MainActivity.kt
+++ app/src/main/java/com/mapbox/MainActivity.kt
@@ -1,72 +1,238 @@
 package com.mapbox
 
+
+import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.drawable.BitmapDrawable
+import android.graphics.drawable.Drawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.MenuItem
 import android.view.Window
 import android.view.WindowManager
-import android.widget.ArrayAdapter
-import android.widget.ListView
-import android.widget.Spinner
+import android.widget.Toast
+import androidx.annotation.DrawableRes
 import androidx.appcompat.app.ActionBarDrawerToggle
-import androidx.core.view.get
+import androidx.appcompat.content.res.AppCompatResources
 import androidx.drawerlayout.widget.DrawerLayout
 import com.google.android.material.navigation.NavigationView
+import com.mapbox.android.gestures.MoveGestureDetector
+import com.mapbox.maps.CameraOptions
+import com.mapbox.maps.ImageHolder
 import com.mapbox.maps.MapView
 import com.mapbox.maps.Style
-import com.mapbox.maps.plugin.Plugin
+import com.mapbox.maps.extension.style.expressions.generated.Expression.Companion.interpolate
+import com.mapbox.maps.plugin.LocationPuck2D
+import com.mapbox.maps.plugin.PuckBearing
+import com.mapbox.maps.plugin.annotation.annotations
+import com.mapbox.maps.plugin.annotation.generated.PointAnnotationOptions
+import com.mapbox.maps.plugin.annotation.generated.createPointAnnotationManager
+import com.mapbox.maps.plugin.gestures.OnMoveListener
+import com.mapbox.maps.plugin.gestures.gestures
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorBearingChangedListener
+import com.mapbox.maps.plugin.locationcomponent.OnIndicatorPositionChangedListener
+import com.mapbox.maps.plugin.locationcomponent.location
+import java.lang.ref.WeakReference
+
 
 class MainActivity : AppCompatActivity() {
 
-    private lateinit var mapView: MapView
-    private lateinit var mapTypeSpinner: Spinner
     private lateinit var drawerLayout: DrawerLayout
-    lateinit var  actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var  actionBarDrawerToggle: ActionBarDrawerToggle
+    private lateinit var locationPermissionHelper: LocationPermissionHelper
 
-    private lateinit var navList: ListView
+    private val onIndicatorBearingChangedListener = OnIndicatorBearingChangedListener {
+        mapView.mapboxMap.setCamera(CameraOptions.Builder().bearing(it).build())
+    }
+
+    private val onIndicatorPositionChangedListener = OnIndicatorPositionChangedListener {
+        mapView.mapboxMap.setCamera(CameraOptions.Builder().center(it).build())
+        mapView.gestures.focalPoint = mapView.mapboxMap.pixelForCoordinate(it)
+    }
+
+    private val onMoveListener = object : OnMoveListener {
+        override fun onMoveBegin(detector: MoveGestureDetector) {
+            onCameraTrackingDismissed()
+        }
+
+        override fun onMove(detector: MoveGestureDetector): Boolean {
+            return false
+        }
+
+        override fun onMoveEnd(detector: MoveGestureDetector) {}
+    }
+    lateinit var mapView: MapView
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
+
         super.onCreate(savedInstanceState)
         // Hide the status bar
         requestWindowFeature(Window.FEATURE_NO_TITLE);
-        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(
+            WindowManager.LayoutParams.FLAG_FULLSCREEN,
+            WindowManager.LayoutParams.FLAG_FULLSCREEN
+        );
 
         setContentView(R.layout.activity_main)
-        mapView=findViewById(R.id.mapView)
+
+
         drawerLayout = findViewById(R.id.drawer_layout)
-        val navigationView: NavigationView = findViewById(R.id.navigation_view)
+        drawerLayout?.let { drawer ->
+            // Perform operations using drawerLayout here
+            val navigationView: NavigationView = findViewById(R.id.navigation_view)
+            actionBarDrawerToggle =
+                ActionBarDrawerToggle(this, drawer, R.string.nav_open, R.string.nav_close)
+            drawer.addDrawerListener(actionBarDrawerToggle)
+            actionBarDrawerToggle.syncState()
 
-        //mapView.getMapboxMap().loadStyleUri(Style.MAPBOX_STREETS)
+            supportActionBar?.setDisplayHomeAsUpEnabled(true)
 
-        // init action bar drawer toggle
-        actionBarDrawerToggle = ActionBarDrawerToggle(this,drawerLayout,R.string.nav_open,R.string.nav_close)
-        // add a drawer listener into  drawer layout
-        drawerLayout.addDrawerListener(actionBarDrawerToggle)
-        actionBarDrawerToggle.syncState()
-
-        // show menu icon and back icon while drawer open
-        supportActionBar?.setDisplayHomeAsUpEnabled(true)
-
-
-        navigationView.setNavigationItemSelectedListener { menuItem ->
-            // Handle menu item clicks here
-            when (menuItem.itemId) {
-                R.id.menu_streets -> {
-                    mapView.getMapboxMap().loadStyleUri(Style.SATELLITE_STREETS)
+            navigationView.setNavigationItemSelectedListener { menuItem ->
+                // Handle menu item clicks here
+                when (menuItem.itemId) {
+                    R.id.menu_streets -> {
+                        mapView.getMapboxMap().loadStyleUri(Style.SATELLITE_STREETS)
+                    }
+                    R.id.menu_satellite -> {
+                        mapView.getMapboxMap().loadStyleUri(Style.SATELLITE)
+                    }
+                    R.id.menu_hybrid -> {
+                        mapView.getMapboxMap().loadStyleUri(Style.OUTDOORS)
+                    }
                 }
-                R.id.menu_satellite -> {
-                    mapView.getMapboxMap().loadStyleUri(Style.SATELLITE)
-                }
-                R.id.menu_hybrid -> {
-                    mapView.getMapboxMap().loadStyleUri(Style.OUTDOORS)
-                }
+                // Close the drawer after handling menu item click
+                drawer.closeDrawers()
+                true
             }
-            // Close the drawer after handling menu item click
-            drawerLayout.closeDrawers()
-            true
+        } ?: kotlin.run {
+            // Handle the case where drawerLayout is null
+            Toast.makeText(this, "DrawerLayout not found", Toast.LENGTH_SHORT).show()
+        }
+        mapView = MapView(this)
+        setContentView(mapView)
+        locationPermissionHelper = LocationPermissionHelper(WeakReference(this))
+        locationPermissionHelper.checkPermissions{
+            onMapReady()
+            addAnnotationToMap()
+
         }
 
 
     }
+
+    private fun onMapReady() {
+        mapView.mapboxMap.setCamera(
+            CameraOptions.Builder()
+                .zoom(14.0)
+                .build()
+        )
+        mapView.mapboxMap.loadStyle(
+            Style.STANDARD
+        ) {
+            initLocationComponent()
+            setupGesturesListener()
+        }
+    }
+
+
+
+    private fun setupGesturesListener() {
+        mapView.gestures.addOnMoveListener(onMoveListener)
+    }
+
+    private fun initLocationComponent() {
+        val locationComponentPlugin = mapView.location
+        locationComponentPlugin.updateSettings {
+            puckBearing = PuckBearing.COURSE
+            puckBearingEnabled = true
+            enabled = true
+            locationPuck = LocationPuck2D(
+                bearingImage = ImageHolder.from(R.drawable.location),
+                shadowImage = ImageHolder.from(R.drawable.gps),
+                scaleExpression = interpolate {
+                    linear()
+                    zoom()
+                    stop {
+                        literal(0.0)
+                        literal(0.6)
+                    }
+                    stop {
+                        literal(20.0)
+                        literal(1.0)
+                    }
+                }.toJson()
+            )
+        }
+        locationComponentPlugin.addOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        locationComponentPlugin.addOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+    }
+
+    private fun onCameraTrackingDismissed() {
+        Toast.makeText(this, "onCameraTrackingDismissed", Toast.LENGTH_SHORT).show()
+        mapView.location
+            .removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        mapView.location
+            .removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+        mapView.gestures.removeOnMoveListener(onMoveListener)
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        mapView.location
+            .removeOnIndicatorBearingChangedListener(onIndicatorBearingChangedListener)
+        mapView.location
+            .removeOnIndicatorPositionChangedListener(onIndicatorPositionChangedListener)
+        mapView.gestures.removeOnMoveListener(onMoveListener)
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        locationPermissionHelper.onRequestPermissionsResult(requestCode, permissions, grantResults)
+    }
+
+    private fun addAnnotationToMap() {
+
+        bitmapFromDrawableRes(
+            this@MainActivity,
+            R.drawable.red_marker
+        )?.let {
+            val annotationApi = mapView.annotations
+            val pointAnnotationManager = annotationApi.createPointAnnotationManager(annotationConfig = null)
+            val pointAnnotationOptions: PointAnnotationOptions = PointAnnotationOptions()
+                .withPoint(com.mapbox.geojson.Point.fromLngLat(18.06, 59.31))
+            pointAnnotationManager?.create(pointAnnotationOptions)
+        }
+    }
+    private fun bitmapFromDrawableRes(context: Context, @DrawableRes resourceId: Int) =
+        convertDrawableToBitmap(AppCompatResources.getDrawable(context, resourceId))
+
+    private fun convertDrawableToBitmap(sourceDrawable: Drawable?): Bitmap? {
+        if (sourceDrawable == null) {
+            return null
+        }
+        return if (sourceDrawable is BitmapDrawable) {
+            sourceDrawable.bitmap
+        } else {
+            val constantState = sourceDrawable.constantState ?: return null
+            val drawable = constantState.newDrawable().mutate()
+            val bitmap: Bitmap = Bitmap.createBitmap(
+                drawable.intrinsicWidth, drawable.intrinsicHeight,
+                Bitmap.Config.ARGB_8888
+            )
+            val canvas = Canvas(bitmap)
+            drawable.setBounds(0, 0, canvas.width, canvas.height)
+            drawable.draw(canvas)
+            bitmap
+        }
+    }
+
+
 
     override fun onOptionsItemSelected(item: MenuItem): Boolean {
         return if (actionBarDrawerToggle.onOptionsItemSelected(item)) {
@@ -74,3 +240,6 @@ class MainActivity : AppCompatActivity() {
         } else super.onOptionsItemSelected(item)
     }
 }
+
+
+
