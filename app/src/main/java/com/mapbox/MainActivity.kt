29f41ac53f0bab7f4b21c70257f329ff9fe1c0e3
vish9431 <vishnu21bcs19@iiitkottayam.ac.in>
2024-03-14 14:39:01 +0530

    Map integrated

--- app/src/main/java/com/mapbox/MainActivity.kt
+++ app/src/main/java/com/mapbox/MainActivity.kt
@@ -2,10 +2,16 @@ package com.mapbox
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.Window
+import android.view.WindowManager
 
 class MainActivity : AppCompatActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        // Hide the status bar
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main)
     }
 }
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -8,8 +8,18 @@ pluginManagement {
 dependencyResolutionManagement {
     repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
     repositories {
+
+        maven {
+            url = uri("https://api.mapbox.com/downloads/v2/releases/maven")
+            credentials.username = "mapbox"
+            credentials.password = providers.gradleProperty("MAPBOX_DOWNLOADS_TOKEN").get()
+            authentication.create<BasicAuthentication>("basic")
+        }
+
+
         google()
         mavenCentral()
+
     }
 }
 
